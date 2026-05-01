8af79a4789d85ad0e51948fc0e7e71a3e408932c
vc-s <sreehaasavc@gmail.com>
2026-05-01 21:25:07 +0530

    Console version + gitignore

---
+++ app.py
@@ -0,0 +1,126 @@
+buckets = {'Transport': {'balance': 680, 'cap': 680}}
+
+def list_buckets():
+    print('\n=============================')
+    print('BUCKETS')
+    print('=============================')
+    for idx, bucket in enumerate(buckets.items()):
+        print(f'{idx + 1}. {bucket[0]}: {bucket[1]}')
+    print('=============================')
+
+def add_bucket(bucket, info):
+    buckets[bucket] = info
+    return f'Added {bucket} as a bucket !!'
+
+def remove_bucket(bucket):
+    if bucket in buckets.keys():
+        buckets.pop(bucket)
+        msg = f'Removed bucket {bucket} !'
+    else:
+        msg = 'Sorry, that bucket does not exist. Please try again !'
+    return msg
+
+def add_to_bucket(bucket, amt):
+    if bucket in buckets:
+        cap = buckets[bucket]['cap']
+        balance = buckets[bucket]['balance']
+        if cap:
+            if amt + balance > cap:
+                return 'Balance exceeds cap !'
+        buckets[bucket]['balance'] += amt
+        return f'Added Rs. {amt} to {bucket} !'
+    else:
+        return 'Sorry, that bucket does not exist. Please try again !'
+
+def remove_from_bucket(bucket, amt):
+    if bucket in buckets:
+        balance = buckets[bucket]['balance']
+        if balance - amt >= 0:
+            buckets[bucket]['balance'] -= amt
+            return f'Removed Rs. {amt} from {bucket} !'
+        else:
+            return 'Balance goes below zero !'
+    else:
+        return 'Sorry, that bucket does not exist. Please try again !'
+
+def transfer(src, dest, amt):
+    src_bal = buckets[src]['balance']
+    dest_bal = buckets[dest]['balance']
+    dest_cap = buckets[dest]['cap']
+    
+    if src_bal - amt < 0:
+        return f'{src} does not have enough balance !'
+    
+    if dest_cap:
+        if dest_bal + amt > dest_cap:
+            return f'Total money in {dest} exceeds its cap !'
+    
+    buckets[src]['balance'] -= amt
+    buckets[dest]['balance'] += amt
+    return f'Transferred {amt} from {src} to {dest} !'
+
+def main():
+    intro = '''
+    Hello, and welcome to the best budgeting app of all time !!
+    Select one of the options below to continue !!'''
+    options = '''
+    1. Add a bucket
+    2. Remove a bucket
+    3. Add money to a bucket
+    4. Remove money from a bucket
+    5. Transfer money from one bucket to another
+    6. List buckets
+    7. Exit'''
+    print(intro)
+    while True:
+        print(options)
+        inp = int(input('\nEnter your option: '))
+        match inp:
+            case 1:
+                name = input('Enter the name of the bucket: ')
+                cap_exists = input('Does this bucket have a cap ? (Y/N): ')
+                if cap_exists.upper() == 'Y': cap = int(input('Enter cap: '))
+                else: cap = None
+                balance = int(input('Enter current balance of the bucket: '))
+                info = {
+                    'balance': balance,
+                    'cap': cap
+                }
+                result = add_bucket(name, info)
+                print(result)
+            
+            case 2:
+                name = input('Enter the name of the bucket: ')
+                result = remove_bucket(name)
+                print(result)
+            
+            case 3:
+                name = input('Enter the name of the bucket: ')
+                amt = int(input('Enter amount to be added to the bucket: '))
+                result = add_to_bucket(name, amt)
+                print(result)
+            
+            case 4:
+                name = input('Enter the name of the bucket: ')
+                amt = int(input('Enter amount to be removed from the bucket: '))
+                result = remove_from_bucket(name, amt)
+                print(result)
+            
+            case 5:
+                src = input('Enter the bucket you want to transfer FROM: ')
+                dest = input('Enter the bucket you want to transfer TO: ')
+                amt = int(input('Enter the amount to be transferred: '))
+                result = transfer(src, dest, amt)
+                print(result)
+            
+            case 6:
+                list_buckets()
+            
+            case 7:
+                print('\nThanks for using our app !!')
+                break
+            
+            case _: print("\nSorry, that's not a valid option ! Try again !!")
+        print('\nWhat do you want to do today ?')
+
+main()
